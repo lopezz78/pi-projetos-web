2c461bc75e0622c1caf082e4ecaca3d7787ce88d
Tuturr-C <arthurgerotto2007@gmail.com>
2025-08-25 09:47:54 -0300

    Delete const numerodasorte = 7.js

--- const numerodasorte = 7.js
+++
@@ -1,8 +0,0 @@
-const numerodasorte = 7
-let randomNumber = Math.floor(Math.random() * 10)+ 1;
-while(numerodasorte !== randomNumber)
-{
-    console.log("Não é o numero :"+ randomNumber);
-    randomNumber = Math.floor(Math.random() * 10)+ 1;
-}
-console.log(" É o numero:"+ randomNumber);
